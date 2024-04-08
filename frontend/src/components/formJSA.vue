b097c66d0e2ea2ae787cdf96a66c403f9279f7b3
Paul R. Carlson <carlsonpar@gmail.com>
2024-04-08 12:50:53 -0400

    updating meta data inputs for the forms

--- frontend/src/components/formJSA.vue
+++ frontend/src/components/formJSA.vue
@@ -1,18 +1,52 @@
 <template>
-  <div>
-    <form>
-      <label for="department">Department</label>
-      <input type="text" name="department" />
-      <label for="preparer">Prepared By:</label>
-      <input type="text" name="preparer" />
-      <label for="date">Date:</label>
-      <input type="text" name="preparer" />
-      <label for="supervisor">Supervisor</label>
-      <input type="text" name="supervisor" />
-      <label for="title">Job Title:</label>
-      <input type="text" name="title" />
-      <label for="activity">Activity</label>
-      <input type="text" name="activity" />
-    </form>
-  </div>
+  <form>
+    <div class="form-row">
+      <div class="form-group col-sm-10">
+        <label for="departmentInput">Department</label>
+        <select class="form-control" id="departmentInput" name="departmentInput">
+          <option>TEST</option>
+          <option>TEST1</option>
+          <option>TEST2</option>
+          <option>TEST3</option>
+        </select>
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="dateInput">Date</label>
+        <input
+          class="form-control"
+          id="dateInput"
+          name="dateInput"
+          type="text"
+          placeholder="date goes here"
+          readonly
+        />
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-sm-6">
+        <label for="supervisorInput">Supervisor</label>
+        <input class="form-control" id="supervisorInput" name="supervisorInput" type="text" />
+      </div>
+      <div class="form-group col-sm-6">
+        <label for="locationInput">Location</label>
+        <input class="form-control" id="locationInput" name="locationInput" type="text" />
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-sm-6">
+        <label for="preparerInput">Preparer</label>
+        <input class="form-control" id="preparerInput" name="preparerInput" type="text" />
+      </div>
+      <div class="form-group col-sm-6">
+        <label for="titleInput">Title</label>
+        <input class="form-control" id="titleInput" name="titleInput" type="text" />
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col">
+        <label for="activityInput">Activity</label>
+        <input class="form-control" id="activityInput" name="titleInput" type="text" />
+      </div>
+    </div>
+  </form>
 </template>
