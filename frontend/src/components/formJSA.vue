48f0e5728970ce0b95d8e1ae0378724be61627ed
Paul R. Carlson <carlsonpar@gmail.com>
2024-04-08 17:06:42 -0400

    updating the form metadata form to populate the supervisor name

--- frontend/src/components/formJSA.vue
+++ frontend/src/components/formJSA.vue
@@ -1,13 +1,18 @@
 <template>
   <form>
+    <h2>Paul</h2>
     <div class="form-row">
       <div class="form-group col-sm-10">
         <label for="departmentInput">Department</label>
-        <select class="form-control" id="departmentInput" name="departmentInput">
-          <option>TEST</option>
-          <option>TEST1</option>
-          <option>TEST2</option>
-          <option>TEST3</option>
+        <select
+          class="form-control"
+          id="departmentInput"
+          name="departmentInput"
+          v-model="departmentName"
+        >
+          <option v-for="dept in department" :key="dept.departmentID">
+            {{ dept.departmentName }}
+          </option>
         </select>
       </div>
       <div class="form-group col-sm-2">
@@ -25,7 +30,14 @@
     <div class="form-row">
       <div class="form-group col-sm-6">
         <label for="supervisorInput">Supervisor</label>
-        <input class="form-control" id="supervisorInput" name="supervisorInput" type="text" />
+        <input
+          class="form-control"
+          id="supervisorInput"
+          name="supervisorInput"
+          readonly
+          :placeholder="`${supervisorName}`"
+          v-model="supervisorName"
+        />
       </div>
       <div class="form-group col-sm-6">
         <label for="locationInput">Location</label>
@@ -50,3 +62,48 @@
     </div>
   </form>
 </template>
+<script lang="ts">
+import axios from 'axios'
+import { defineComponent } from 'vue'
+import type { Associate, Department } from '../types'
+export default defineComponent({
+  data() {
+    return {
+      departmentName: '',
+      supervisorName: '' as String,
+      department: [] as Department[],
+      people: [] as Associate[]
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      console.log('fetch data')
+      axios
+        .get('http://localhost/departments.php') // Assuming your PHP backend is running on port 8081
+        .then((response) => {
+          console.log(typeof response.data)
+          this.department = response.data[0]
+          this.people = response.data[1]
+          console.log(response.data[0])
+          console.log('-------')
+          console.log(response.data[1])
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error)
+        })
+    }
+  },
+  watch: {
+    departmentName: function (newname, oldname) {
+      console.log(newname)
+
+      this.department
+        .filter((record) => record.departmentName == newname)
+        .forEach((p) => (this.supervisorName = p.associateName))
+    }
+  }
+})
+</script>
