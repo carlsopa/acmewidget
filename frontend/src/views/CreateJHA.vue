346abc43fa57a0723e10de9800a70f3eb70fe6e0
Paul R. Carlson <carlsonpar@gmail.com>
2024-04-11 11:46:32 -0400

    final tweaks

--- frontend/src/views/CreateJHA.vue
+++ frontend/src/views/CreateJHA.vue
@@ -2,6 +2,9 @@
   <container>
     <formJSA @activate="stepActivate" />
     <createStep v-if="step" />
+    <div v-if="step" style="display: flex; justify-content: center; margin-top: 30px">
+      <button @click="clickie()" class="btn btn-info" style="font-size: 25px">Finish form</button>
+    </div>
   </container>
 </template>
 <script setup lang="ts">
@@ -9,10 +12,15 @@ import formJSA from '../components/formJSA.vue'
 import createStep from '../components/createStep.vue'
 import { ref } from 'vue'
 import { store } from '../store/store.js'
+import { useRouter } from 'vue-router'
 
+const route = useRouter()
 const step = ref(false)
 const stepActivate = () => {
   console.log('second good')
   step.value = true
+}
+function clickie() {
+  route.push({ path: '/view' })
 }
 </script>
